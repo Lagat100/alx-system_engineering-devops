fb3ef07763ca4b03e161beb7a239f55746e91735
Lagat100 <weldonkiprop1@gmail.com>
2024-05-09 14:16:14 -0700

    fix top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,14 +7,16 @@ from sys import argv
 
 
 def top_ten(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
-    params = {'limit': 10}
-    response = requests.get(url, headers=headers, params=params)
+    response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
         data = response.json()
-        for post in data['data']['children']:
-            print(post['data']['title'])
+        if 'data' in data and 'children' in data['data']:
+            for post in data['data']['children']:
+                print(post['data']['title'])
+        else:
+            print(None)
     else:
         print(None)
